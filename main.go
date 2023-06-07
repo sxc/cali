b089db8d6bce6b016261af511d018b88b000c44a
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-06-07 14:39:40 +0800

    add mongodb connect

--- main.go
+++ main.go
@@ -1,29 +1,56 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/sxc/cali/api"
+	"github.com/sxc/cali/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// const dburi = "mongodb://localhost:27017"
+const dburi = "mongodb://root:password@localhost:27017/"
+const dbname = "cali"
+const userColl = "users"
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+	user := types.User{
+		FirstName: "John",
+		LastName:  "Appleseed",
+	}
+
+	_, err = coll.InsertOne(ctx, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var john types.User
+
+	if err := coll.FindOne(ctx, bson.M{}).Decode(&john); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(john)
+
 	listenAddr := flag.String("listenAddr", ":4000", "The listen address of the API server")
 	flag.Parse()
 	app := fiber.New()
 	apiv1 := app.Group("/api/v1")
 
-	// app.Get("/foo", handleFoo)
-	// rebuild the application
 	apiv1.Get("/user", api.HandleGetUsers)
 	apiv1.Get("/user/:id", api.HandleGetUserByID)
 	app.Listen(*listenAddr)
 }
-
-// func handleFoo(c *fiber.Ctx) error {
-// 	return c.JSON(map[string]string{"msg": "working just fine!"})
-// }
-
-// func handleUser(c *fiber.Ctx) error {
-// 	return c.JSON(map[string]string{"user": "John Appleseed"})
-// }
